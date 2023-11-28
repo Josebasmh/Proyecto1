551b431d0774ac102e72b0b6a55be1b62614a78a
Joseba <117350714+Josebasmh@users.noreply.github.com>
2023-11-28 12:43:42 +0100

    ADD-TablaGeneral y TablaGeneralController
    añadido el fxml de la tabla principal (Participación) y su controlador sin editar, solo insertando el esqueleto.

---
+++ src/controller/TablaGeneralController.java
@@ -0,0 +1,89 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Menu;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class TablaGeneralController {
+
+    @FXML
+    private ChoiceBox<?> cbBusqueda;
+
+    @FXML
+    private Menu mAyuda;
+
+    @FXML
+    private Menu mAñadir;
+
+    @FXML
+    private Menu mTablas;
+
+    @FXML
+    private TableColumn<?, ?> tcDeporte;
+
+    @FXML
+    private TableColumn<?, ?> tcDeportista;
+
+    @FXML
+    private TableColumn<?, ?> tcEdad;
+
+    @FXML
+    private TableColumn<?, ?> tcEquipo;
+
+    @FXML
+    private TableColumn<?, ?> tcEvento;
+
+    @FXML
+    private TableColumn<?, ?> tcMedalla;
+
+    @FXML
+    private TableColumn<?, ?> tcOlimpiada;
+
+    @FXML
+    private TextField tfBusqueda;
+
+    @FXML
+    void abrirTabla(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aniadirDeportista(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aniadirEquipo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aniadirEvento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aniadirOlimpiada(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aniadirParticipacion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void filtrar(KeyEvent event) {
+
+    }
+
+    @FXML
+    void ventanaAyuda(ActionEvent event) {
+
+    }
+
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,21 @@
 /**
- * 
- */
-/**
  * @author Joseba
  *
  */
 module Proyecto1 {
+	requires javafx.controls;
+	requires java.desktop;
+	requires javafx.web;
+	requires javafx.fxml;
+	requires javafx.swing;
+	requires javafx.media;
+	requires javafx.graphics;
+	requires javafx.base;
+	requires java.sql;
+	
+	opens model to javafx.graphics, javafx.fxml, javafx.base;
+	opens application to javafx.graphics, javafx.fxml, javafx.base;
+	opens controller to javafx.graphics, javafx.fxml, javafx.base;
+	opens dao to javafx.base,java.sql;
+	opens conexion to java.sql;
 }
