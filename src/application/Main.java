aa4c1cb168768b92a0551a93c4edcda8d6a12bc3
Joseba <117350714+Josebasmh@users.noreply.github.com>
2023-12-27 16:44:15 +0100

    ADD-main e icono
    Añadido el main para iniciar la aplicación e insertada imagen como logo.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,39 @@
 package application;
 
-public class Main {
+import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application{
+	
+	FlowPane root;
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		try {
+			root = (FlowPane)FXMLLoader.load(getClass().getResource("/fxml/Ventanageneral.fxml"));
+			stage.setTitle("GESTION DE OLIMPIADAS");
+			Scene scene = new Scene(root,800,600);
+			stage.setScene(scene);
+			stage.setMinWidth(800);
+			stage.setMinHeight(600);
+			stage.setMaxWidth(800);
+			stage.setMaxHeight(600);
+			stage.getIcons().add(new Image(getClass().getResource("/img/imgOlimpiadas.jpg").toString()));
+			stage.show();	
+		}catch(IOException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
 }
